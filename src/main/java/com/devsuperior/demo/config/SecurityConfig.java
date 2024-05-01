96c408a4123abd70d8180a120f37f819a95b825f
Diego Santana <3c.santana@gmail.com>
2024-04-30 22:43:21 -0300

    feat: implementa o BCrypt

--- src/main/java/com/devsuperior/demo/config/SecurityConfig.java
+++ src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -3,17 +3,25 @@ package com.devsuperior.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     
     @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // desable proteção contra ataques csrf (seção)
         http.csrf(csrf -> csrf.disable());
-        // permite acesso a todos os endpointe
+        // permite acesso a todos os endpoints
         http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
 }
+
